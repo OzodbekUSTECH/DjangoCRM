b318aee5276013708679b2cadcef3c3f06c2857d
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-05-09 19:33:52 +0500

    form for creating a record

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -64,4 +64,18 @@ def delete_record(request, pk):
         return redirect('home')
     else:
         messages.success(request, 'You must be logged in to do that function!')
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                #add_record = form.save() #idk
+                form  = form.save()
+                messages.success(request, 'Record added Successfully!')
+                return redirect('home')
+        return render(request, 'add_record.html', { 'form': form })
+    else:
+        messages.success(request, 'You must be logged in!')
         return redirect('home')
