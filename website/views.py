b2a43839228079838a3c67633cabb2e713078fc7
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-05-09 19:16:29 +0500

    delete record

--- website/views.py
+++ website/views.py
@@ -56,3 +56,12 @@ def customer_record(request, pk):
         messages.success(request, 'You must be logged in to view that page!')
         return redirect('home')
         
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Record Deleted Successfully.')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to do that function!')
+        return redirect('home')
