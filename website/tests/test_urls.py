d40ea4fc7ad3f41e4403859d3736d9a3f8f81ac0
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-05-10 23:04:12 +0500

    tests

---
+++ website/tests/test_urls.py
@@ -0,0 +1,29 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from website.views import  home, logout, register_user, customer_record, add_record, delete_record, update_record
+
+class TestUrls(SimpleTestCase):
+
+    def  test_home_url(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, home)
+
+    def  test_reg_url(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func, register_user)
+
+    def  test_record_url(self):
+        url = reverse('record', args=['3'])
+        self.assertEquals(resolve(url).func, customer_record)
+
+    def  test_add_record_url(self):
+        url = reverse('add_record')
+        self.assertEquals(resolve(url).func, add_record)
+
+    def  test_delete_record_url(self):
+        url = reverse('delete_record', args=['2'])
+        self.assertEquals(resolve(url).func, delete_record)
+
+    def  test_update_record_url(self):
+        url = reverse('update_record', args=['2'])
+        self.assertEquals(resolve(url).func, update_record)
---
+++ website/tests/test_views.py
@@ -0,0 +1,56 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from website.models import Record
+import json
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.record_url = reverse('home')
+        self.add_record_url = reverse('add_record')
+        # self.detail_url = reverse('record', args=[11])
+        # self.record1 = Record.objects.create(
+        #     first_name = 'john',
+        #     last_name = 'doe',
+        #     email = 'john.doe@example.com',
+        #     phone = '5555555',
+        #     address = 'dojwakdwa',
+        #     city = 'San Francisco',
+        #     state = 'CA',
+        #     zipcode = '94'
+        # )
+
+
+
+    def test_record_list_GET(self):
+
+        response = self.client.get(self.record_url)
+
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'home.html')
+    
+    # ERROR 302
+    # def test_record_detail_GET(self):
+    #     response = self.client.get(self.detail_url)
+
+    #     self.assertEquals(response.status_code, 200)
+
+    #     self.assertTemplateUsed(response, 'record.html')
+
+    def test_record_detail_POST(self):
+       
+
+        response = self.client.post(self.add_record_url, {     
+            'first_name': 'john',
+            'last_name': 'doe',
+            'email': 'john.doe@example.com',
+            'phone': '5555555', 
+            'address': 'dojwakdwa',
+            'city': 'San Francisco',
+            'state': 'CA',
+            'zipcode': '94' 
+        })
+
+        self.assertEquals(response.status_code, 302)
+        # self.assertEquals()
