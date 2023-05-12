b9d1553624172250c4b463527d851dd155be50e4
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-05-12 17:47:29 +0500

    test models

--- website/tests/test_API.py
+++ website/tests/test_API.py
@@ -1,167 +1,167 @@
-from rest_framework import status, reverse
-from rest_framework.test import APITestCase, APIClient
-from website.models import Record
-# new one 
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from API.views import *
+# from rest_framework import status, reverse
+# from rest_framework.test import APITestCase, APIClient
+# from website.models import Record
+# # new one 
+# from django.test import SimpleTestCase
+# from django.urls import reverse, resolve
+# from API.views import *
 
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework import status
+# from django.contrib.auth.models import User
+# from rest_framework.authtoken.models import Token
+# from rest_framework import status
 
-# class ApiUrlsTests(SimpleTestCase):
+# # class ApiUrlsTests(SimpleTestCase):
     
-    # def test_get_customers_is_resolved(self):
-    #     url = '/api/v1/Users/'
-    #     # print(resolve(url).func.__module__)
-    #     self.assertEquals(resolve(url), RecordViewSet)
+#     # def test_get_customers_is_resolved(self):
+#     #     url = '/api/v1/Users/'
+#     #     # print(resolve(url).func.__module__)
+#     #     self.assertEquals(resolve(url), RecordViewSet)
 
-class RecordAPIViewTests(APITestCase):
+# class RecordAPIViewTests(APITestCase):
 
-    records_url = '/api/v1/Users/'
+#     records_url = '/api/v1/Users/'
 
-    def setUp(self):
-        self.user = User.objects.create_user(username='ozodbi4ka', password='77girado')
-        self.token = Token.objects.create(user=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+#     def setUp(self):
+#         self.user = User.objects.create_user(username='ozodbi4ka', password='77girado')
+#         self.token = Token.objects.create(user=self.user)
+#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-    def tearDown(self):
-        pass
+#     def tearDown(self):
+#         pass
 
-    def test_get_users_authenticated(self):
-        response = self.client.get(self.records_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+#     def test_get_users_authenticated(self):
+#         response = self.client.get(self.records_url)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         print(response.data)
+# #     def test_get_users_un_authenticated(self):
+# #         self.client.force_authenticate(user=None, token=None)
+# #         response = self.client.get(self.records_url)
+# #         self.assertEqual(response.status_code, 401)
 
-    def test_get_users_un_authenticated(self):
-        self.client.force_authenticate(user=None, token=None)
-        response = self.client.get(self.records_url)
-        self.assertEqual(response.status_code, 401)
-
-    def test_post_users_authenticated(self):
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "johndoe@example.com",
-            "phone": "1234567890",
-            "address": "123 Main St",
-            "city": "Anytown",
-            "state": "CA",
-            "zipcode": "12345"
-        }
-        response = self.client.post(self.records_url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['first_name'], 'John')
+# #     def test_post_users_authenticated(self):
+# #         data = {
+# #             "first_name": "John",
+# #             "last_name": "Doe",
+# #             "email": "johndoe@example.com",
+# #             "phone": "1234567890",
+# #             "address": "123 Main St",
+# #             "city": "Anytown",
+# #             "state": "CA",
+# #             "zipcode": "12345"
+# #         }
+# #         response = self.client.post(self.records_url, data, format='json')
+# #         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+# #         self.assertEqual(response.data['first_name'], 'John')
 
 
-class UserDetailViewTest(APITestCase):
+# # class UserDetailViewTest(APITestCase):
 
-    users_url = '/ap1/v1/Users/'
-    user_url = reverse('record-detail', args=[2])
+# #     users_url = '/ap1/v1/Users/'
+# #     user_url = reverse('record-detail', args=[2])
     
-    def setUp(self):
-        self.user = User.objects.create_user(username='ozodbi4ka', password='77girado')
-        self.token = Token.objects.create(user=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+# #     def setUp(self):
+# #         self.user = User.objects.create_user(username='ozodbi4ka', password='77girado')
+# #         self.token = Token.objects.create(user=self.user)
+# #         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-        #Saving user
-        data = {
-            "first_name": "better",
-            "last_name": "Ozod",
-            "email": "johndoe@example.com",
-            "phone": "1234567890",
-            "address": "123 Main St",
-            "city": "Anytown",
-            "state": "CA",
-            "zipcode": "12345"
-        }
-        self.client.post(self.users_url, data, format='json')
-        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+# #         #Saving user
+# #         data = {
+# #             "first_name": "better",
+# #             "last_name": "Ozod",
+# #             "email": "johndoe@example.com",
+# #             "phone": "1234567890",
+# #             "address": "123 Main St",
+# #             "city": "Anytown",
+# #             "state": "CA",
+# #             "zipcode": "12345"
+# #         }
+# #         self.client.post(self.users_url, data, format='json')
+# #         # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    #СРАБОТАЛ И УМЕР!!!!!
-    # def test_get_user_authenticated(self):
-    #     response = self.client.get(self.user_url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.data['first_name'], 'better')
+# #     #СРАБОТАЛ И УМЕР!!!!!
+# #     # def test_get_user_authenticated(self):
+# #     #     response = self.client.get(self.user_url)
+# #     #     self.assertEqual(response.status_code, 200)
+# #     #     self.assertEqual(response.data['first_name'], 'better')
     
-    def test_get_user_un_authenticated(self):
-        self.client.force_authenticate(user=None, token=None)
-        response = self.client.get(self.user_url)
-        self.assertEqual(response.status_code, 401)
-        
-     #НЕ РАБОТАЕТ!!!   
-    # def test_delete_user_authenticated(self):
-    #     response = self.client.delete(self.user_url)
-    #     self.assertEqual(response.status_code, 204)
+# #     def test_get_user_un_authenticated(self):
+# #         self.client.force_authenticate(user=None, token=None)
+# #         response = self.client.get(self.user_url)
+# #         self.assertEqual(response.status_code, 401)
+
+#      #НЕ РАБОТАЕТ!!!   
+#     # def test_delete_user_authenticated(self):
+#     #     response = self.client.delete(self.user_url)
+#     #     self.assertEqual(response.status_code, 204)
 
 
 
 
-# class RecordAPITestCase(APITestCase):
-    # def setUp(self):
-    #     Record.objects.create(
-    #         first_name = "вцфвцфвцф",
-    #         last_name = "Doe",
-    #         email = "johndoe@example.com",
-    #         phone = "1234567890",
-    #         address = "123 Main St",
-    #         city = "Anytown",
-    #         state = "CA",
-    #         zipcode = "12345"
-    #     )
-    #     Record.objects.create(
-    #         first_name = "ozod",
-    #         last_name = "dwadwa",
-    #         email = "dwaddawdaw@example.com",
-    #         phone = "23132131",
-    #         address = "123 dwadaw St",
-    #         city = "dwadaw",
-    #         state = "dwadaw",
-    #         zipcode = "dwadaw"
-    #     )
+# # class RecordAPITestCase(APITestCase):
+#     # def setUp(self):
+#     #     Record.objects.create(
+#     #         first_name = "вцфвцфвцф",
+#     #         last_name = "Doe",
+#     #         email = "johndoe@example.com",
+#     #         phone = "1234567890",
+#     #         address = "123 Main St",
+#     #         city = "Anytown",
+#     #         state = "CA",
+#     #         zipcode = "12345"
+#     #     )
+#     #     Record.objects.create(
+#     #         first_name = "ozod",
+#     #         last_name = "dwadwa",
+#     #         email = "dwaddawdaw@example.com",
+#     #         phone = "23132131",
+#     #         address = "123 dwadaw St",
+#     #         city = "dwadaw",
+#     #         state = "dwadaw",
+#     #         zipcode = "dwadaw"
+#     #     )
 
-    # def test_record_list(self):
-    #     response = self.client.get('/api/v1/Users/')
-    #     # print(response.data.results)
-    #     # self.assertEqual(response.data, {'id': 2, 'first_name': 'ozod'})
-    #     self.assertTrue({'id': 2, 'first_name': 'ozod'} in response.data)
+#     # def test_record_list(self):
+#     #     response = self.client.get('/api/v1/Users/')
+#     #     # print(response.data.results)
+#     #     # self.assertEqual(response.data, {'id': 2, 'first_name': 'ozod'})
+#     #     self.assertTrue({'id': 2, 'first_name': 'ozod'} in response.data)
 
 
 
-    # def setUp(self):
-    #     self.valid_payload = {
-    #         "first_name": "вцфвцфвцф",
-    #         "last_name": "Doe",
-    #         "email": "johndoe@example.com",
-    #         "phone": "1234567890",
-    #         "address": "123 Main St",
-    #         "city": "Anytown",
-    #         "state": "CA",
-    #         "zipcode": "12345"
-    #     }
-    #     self.invalid_payload = {
-    #         "first_name": "",
-    #         "last_name": "",
-    #         "email": "",
-    #         "phone": "",
-    #         "address": "",
-    #         "city": "",
-    #         "state": "",
-    #         "zipcode": ""
-    #     }
-    #     Record.objects.create(**self.valid_payload)
+#     # def setUp(self):
+#     #     self.valid_payload = {
+#     #         "first_name": "вцфвцфвцф",
+#     #         "last_name": "Doe",
+#     #         "email": "johndoe@example.com",
+#     #         "phone": "1234567890",
+#     #         "address": "123 Main St",
+#     #         "city": "Anytown",
+#     #         "state": "CA",
+#     #         "zipcode": "12345"
+#     #     }
+#     #     self.invalid_payload = {
+#     #         "first_name": "",
+#     #         "last_name": "",
+#     #         "email": "",
+#     #         "phone": "",
+#     #         "address": "",
+#     #         "city": "",
+#     #         "state": "",
+#     #         "zipcode": ""
+#     #     }
+#     #     Record.objects.create(**self.valid_payload)
 
-    # def test_create_valid_record(self):
-    #     response = self.client.post('/api/v1/Users/', self.valid_payload, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(Record.objects.count(), 2)
+#     # def test_create_valid_record(self):
+#     #     response = self.client.post('/api/v1/Users/', self.valid_payload, format='json')
+#     #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#     #     self.assertEqual(Record.objects.count(), 2)
 
-    # def test_create_invalid_record(self):
-    #     response = self.client.post('/api/v1/Users/', self.invalid_payload, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertEqual(Record.objects.count(), 1)
+#     # def test_create_invalid_record(self):
+#     #     response = self.client.post('/api/v1/Users/', self.invalid_payload, format='json')
+#     #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#     #     self.assertEqual(Record.objects.count(), 1)
 
-    # def test_get_all_records(self):
-    #     response = self.client.get('/api/v1/Users/')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(len(response.data), 1)
+#     # def test_get_all_records(self):
+#     #     response = self.client.get('/api/v1/Users/')
+#     #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+#     #     self.assertEqual(len(response.data), 1)
---
+++ website/tests/test_models.py
@@ -0,0 +1,18 @@
+# from django.test import TestCase
+# from website.models import Record
+
+# class TestModel(TestCase):
+
+#     def test_create_record(self):
+#         record = Record.objects.create(
+#             first_name = "John",
+#             last_name = "Doe",
+#             email = "johndoe@example.com",
+#             phone = "1234567890",
+#             address = "123 Main St",
+#             city = "Anytown",
+#             state = "CA",
+#             zipcode = "12345"
+#         )
+#         record.save()
+#         self.assertEqual(str(record), 'John')
